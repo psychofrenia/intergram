d84ee25c5549b9b726fd3a90f3326aaea23251e4
Wiwin Hartanto <me@wiwinhartanto.com>
2018-04-15 08:09:57 +0700

    Update chat.js

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -41,9 +41,7 @@ export default class Chat extends Component {
                        ref={(input) => { this.input = input }}
                        onKeyPress={this.handleKeyPress}/>
 
-                <a class="banner" href="https://github.com/idoco/intergram" target="_blank">
-                    Powered by <b>Intergram</b>&nbsp;
-                </a>
+                
             </div>
         );
     }
